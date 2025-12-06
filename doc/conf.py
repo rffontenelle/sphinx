abc1cfa2dc1b7e8707892351238903e60f7ca9c6
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2025-12-05 21:46:01 -0300

    Set LaTeX engine to xelatex in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -76,6 +76,7 @@ epub_show_urls = 'inline'
 epub_use_index = False
 epub_description = 'Sphinx documentation generator system manual'
 
+latex_engine = 'xelatex'
 latex_documents = [
     (
         'index',
